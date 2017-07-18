71cbcdfec6161e116d30dc7fbd94968a70929944
BuildTools <unconfigured@null.spigotmc.org>
2017-07-18 14:08:10 -0400

    Finished and organized First Level Begin Second level using prefabs

---
+++ Assets/Scripts/Ball.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour {
+    public Paddle paddle;
+    private bool hasStarted = false;
+    private Vector3 paddletoBallVector;
+
+	// Use this for initialization
+	void Start () {
+        paddletoBallVector = this.transform.position - paddle.transform.position;
+        print(paddletoBallVector.y);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        //print(paddle.GetComponent<Rigidbody2D>().velocity.magnitude);
+        if (!hasStarted)
+        {
+            this.transform.position = paddle.transform.position + paddletoBallVector;
+            if (Input.GetMouseButtonDown(0))
+            {
+                print("Launch ball");
+                //print("CLICK: " + paddle.GetComponent<Rigidbody2D>().velocity);
+                this.GetComponent<Rigidbody2D>().velocity =  new Vector2(10 , 10f);
+                hasStarted = true;
+            }
+        }
+	}
+}
